80e329129b3a7d7de8744a24b59900fa5960ff13
chemicalbondo <chemicalbondo@gmail.com>
2020-09-22 07:07:48 +0900

    アイコンとアイコンの下の文字列を追加

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -3,8 +3,18 @@ import 'package:flutter/material.dart';
 class TasksScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('data'),
+    return Scaffold(
+      backgroundColor: Colors.lightBlue,
+      body: Container(
+        padding:
+            EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0 ),
+        child: Column(
+          children: [
+            Icon(Icons.list),
+            Text('Todoey'),
+          ],
+        ),
+      ),
     );
   }
 }
