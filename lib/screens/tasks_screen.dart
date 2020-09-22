53e0f993033cba414849cb1eb5a730217a494d18
chemicalbondo <chemicalbondo@gmail.com>
2020-09-23 06:10:07 +0900

    tasks_screenのリストビュー途中まで作成

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -7,11 +7,48 @@ class TasksScreen extends StatelessWidget {
       backgroundColor: Colors.lightBlue,
       body: Container(
         padding:
-            EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0 ),
+            EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Icon(Icons.list),
-            Text('Todoey'),
+            CircleAvatar(
+              child: Icon(
+                Icons.list,
+                size: 30.0,
+                color: Colors.lightBlue,
+              ),
+              backgroundColor: Colors.white,
+              radius: 30.0,
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'Todoey',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 50.0,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Text(
+              '12 Tasks',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18.0,
+              ),
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20.0),
+                    topRight: Radius.circular(20.0),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
