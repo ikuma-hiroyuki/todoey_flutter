a7d14fa252031506ac6c6b14f21974c3c3afecb5
ikuma-hiroyuki <chemicalbondo@gmail.com>
2020-09-25 08:57:11 +0900

    タスクリストをtasks_listにリフトアップ

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -3,32 +3,29 @@ import 'package:todoey_flutter/models/task.dart';
 import 'package:todoey_flutter/widgets/task_title.dart';
 
 class TasksList extends StatefulWidget {
+  final List<Task> tasks;
+  TasksList(this.tasks);
+
   @override
   _TasksListState createState() => _TasksListState();
 }
 
 class _TasksListState extends State<TasksList> {
-  List<Task> tasks = [
-    Task(name: 'Buy milk'),
-    Task(name: 'Buy eggs'),
-    Task(name: 'Buy bread'),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemBuilder: (context, index) {
         return TaskTitle(
-          taskTitle: tasks[index].name,
-          isChecked: tasks[index].isDone,
+          taskTitle: widget.tasks[index].name,
+          isChecked: widget.tasks[index].isDone,
           checkboxCallback: (bool checkboxState) {
             setState(() {
-              tasks[index].toggleDone();
+              widget.tasks[index].toggleDone();
             });
           },
         );
       },
-      itemCount: tasks.length,
+      itemCount: widget.tasks.length,
     );
   }
 }
