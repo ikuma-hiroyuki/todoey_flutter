0acb77554222602e8e616917fc9539245cf7ba90
ikuma-hiroyuki <chemicalbondo@gmail.com>
2020-09-26 12:56:06 +0900

    ChangeNotifierの実装

---
+++ lib/models/task_data.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart';
+import 'package:todoey_flutter/models/task.dart';
+
+class TaskData extends ChangeNotifier {
+  List<Task> tasks = [
+    Task(name: 'Buy milk'),
+    Task(name: 'Buy eggs'),
+    Task(name: 'Buy bread'),
+    Task(name: 'Buy cake'),
+  ];
+}
